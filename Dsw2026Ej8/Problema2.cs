8af8e92fb494a5a2409f69ad294f0365565448d5
matiaseas <matias.eas.unt@gmail.com>
2026-05-03 20:09:47 -0300

    Realizado Problema n°2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long _productCode, string _productDescription, int _quantity, decimal _unitPrice)
+        {
+            var venta = new {
+                Code = _productCode,
+                Description = _productDescription,
+                Quantity = _quantity,
+                Total = _quantity > 0 ? _quantity * _unitPrice : 0
+            };
+
+            return $"{venta.Code} - {venta.Description} - {venta.Total:C}";
+        }
+    }
+}
