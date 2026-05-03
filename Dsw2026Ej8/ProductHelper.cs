3376ef93a01739ef1f1568114a2167e1cdc6ae67
matiaseas <matias.eas.unt@gmail.com>
2026-05-03 19:51:55 -0300

    Realizado Problema n°1

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+
+        public string ObtenerEtiquetaProducto (long _code, string _description, decimal _price)
+        {
+            return $"[{_code}] {_description} - {FormatearPrecio(_price)}";
+        }
+
+    }
+}
---
+++ Dsw2026Ej8/ProductHelper2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+
+        public string FormatearPrecio (decimal _price)
+        {
+            return $"${_price}";
+        }
+
+    }
+}
