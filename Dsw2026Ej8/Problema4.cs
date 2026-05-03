d3d9ac0d2328874001e300ab9cfa05af620afb21
matiaseas <matias.eas.unt@gmail.com>
2026-05-03 20:57:53 -0300

    Realizado Problema n°4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int[] notasValidas =
+            [
+            nota1 ?? int.MinValue,
+            nota2 ?? int.MinValue,
+            nota3 ?? int.MinValue
+            ];
+
+            var filtradas = notasValidas
+                .Where(nota => nota >= 0 && nota <= 10)
+                .ToArray();
+
+            if (filtradas.Length == 0)
+            {
+                return 0;
+            }
+
+            return filtradas.Average();
+        }
+    }
+}
