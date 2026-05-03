2e6aa316f13df31a9fb0dd58f3e9b278c01f93e3
matiaseas <matias.eas.unt@gmail.com>
2026-05-03 20:38:35 -0300

    Realizado Problema n°3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        public string CompararCopias (int originalValue, Product product)
+        {
+            int copyValue = originalValue;
+            copyValue++;
+
+            Product copyProduct = product;
+            copyProduct.ChangeDescription("Descripción cambiada");
+
+            return $"{originalValue}-{copyValue}-{product._description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string _description;
+
+        public Product(string description)
+        {
+            _description = description;
+        }
+
+        public void ChangeDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+    }
+}
