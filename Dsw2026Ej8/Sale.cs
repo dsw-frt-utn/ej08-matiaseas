f54698e502e00fc86c722aa88a4e275d6d67bb48
matiaseas <matias.eas.unt@gmail.com>
2026-05-03 21:25:41 -0300

    Realizado Problema n°5

---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        public decimal Amount;
+
+        public virtual decimal CalculateTotal()
+        {
+            return Amount;
+        }
+    }
+}
---
+++ Dsw2026Ej8/WholesaleSale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return Amount * 0.9m;
+        }
+
+    }
+}
